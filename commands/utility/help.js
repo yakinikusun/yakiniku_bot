35de3384fea9cf2fefe7031280494d7700830702
yakinikusun <yakinikudaisuki01@gmail.com>
2026-09-04 07:32:14 +0000

    add: helpに情報を追加

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -7,8 +7,14 @@ module.exports = {
 
 	async execute(interaction) {
 		dicehelpEmbed
-			.setDescription("BCDiceを利用したDiscord用のダイスボットです")
+			.setDescription("カス機能が沢山入ってるボットです")
 			.setFields(
+				{ name: "鳥検知", value: "以下のものを送信した場合、鳥扱いします\n『うぉ\*, うお, うおっ, うおw, うおW, おお, おぉ』", inline: false },)
+			.addFields(
+				{ name: "自動冷笑", value: "1/100の確率で冷笑します\n1/10000の確率で特大冷笑します", inline: false },)
+			.addFields(
+				{ name: "コマンド一覧", value: "", inline: false },)
+			.addFields(
 				{ name: "/dice", value: "ダイスを振ることができます", inline: true },
 				{ name: "/d", value: "\*\*/dice\*\*の短縮版です", inline: true },
 				{ name: "", value: "", inline: false },
